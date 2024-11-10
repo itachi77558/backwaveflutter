81eeb921a151424c67f888996c1cdf1cf241f71c
Papa Faly <pfdiagne@gmail.com>
2024-11-10 02:57:42 +0000

    odod

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,28 +10,32 @@ use Illuminate\Support\Facades\Validator;
 class UserController extends Controller
 {
     public function createAccount(Request $request)
-{
-    $validator = Validator::make($request->all(), [
-        'first_name' => 'required|string|max:255',
-        'last_name' => 'required|string|max:255',
-        'email' => 'required|email|unique:users',
-        'phone_number' => 'required|string|unique:users',
-        'password' => 'required|string|min:6|confirmed',
-    ]);
+    {
+        // Valider les données d'entrée
+        $validator = Validator::make($request->all(), [
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users',
+            'phone_number' => 'required|string|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
 
-    if ($validator->fails()) {
-        return response()->json(['error' => $validator->errors()], 400);
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 400);
+        }
+
+        // Créer un utilisateur
+        $user = User::create([
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'email' => $request->email,
+            'phone_number' => $request->phone_number,
+            'password' => Hash::make($request->password), // Hashage du mot de passe
+        ]);
+
+        logger('Requête reçue : ', $request->all());
+
+
+        return response()->json(['message' => 'Account created successfully', 'user' => $user], 201);
     }
-
-    $user = User::create([
-        'first_name' => $request->first_name,
-        'last_name' => $request->last_name,
-        'email' => $request->email,
-        'phone_number' => $request->phone_number,
-        'password' => Hash::make($request->password),
-    ]);
-
-    return response()->json(['message' => 'Account created successfully', 'user' => $user], 201);
-}
-
 }
