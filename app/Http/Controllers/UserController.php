583128fcffec14d179f07f97e5ead1aa05aa6637
Papa Faly <pfdiagne@gmail.com>
2024-11-12 17:28:43 +0000

    gda

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -285,7 +285,7 @@ class UserController extends Controller
     }
 
 
-    public function profile(Request $request)
+    public function getProfile(Request $request)
 {
     $user = $request->user();
 
